acd710f153e301829d6db4f9e41980a562707279
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2025-07-26 21:24:42 -0400

    Commited

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from vertexai.preview.generative_models import (
     GenerativeModel,
-    GenerativeConfig,
+
 )
 from dotenv import load_dotenv
 import os
@@ -13,12 +13,14 @@ load_dotenv()
 project_id = os.getenv("project_id")
 region = os.getenv("region")
 
+
+
 #Authenticate with Google Cloud
 vertexai.init(project=project_id, location=region)
 
 
 #load the model
-model_name="gemini-2.0-flash-001"
+model_name=GenerativeModel("gemini-2.0-flash-001")
 
 def user_interfaces():
     st.set_page_config("Vertex AI")
@@ -31,4 +33,9 @@ def user_interfaces():
         response = model_name.generate_content(user_question,stream=True)
 
         for res in response:
-            st.write(res.text,end="")
+            st.write(res.text,end=" ")
+
+    
+if __name__ == "__main__":
+    user_interfaces()
+
